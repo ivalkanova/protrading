42ef59be34ae5040b42b2370bca5b475fb8d020a
Stanislav Stefanov <stanislavsvetlinovstefanov@gmail.com>
2020-06-15 08:42:40 +0300

    Add TestIdentifier class. Add TestIdentifier field to Strategy test. Add get methods, equals and hashCode to StrategyTest.

--- src/main/java/com/trading/protrading/backtesting/StrategyTest.java
+++ src/main/java/com/trading/protrading/backtesting/StrategyTest.java
@@ -1,32 +1,83 @@
 package com.trading.protrading.backtesting;
 
 import com.trading.protrading.data.strategy.Asset;
-import com.trading.protrading.data.strategy.ReportType;
 import com.trading.protrading.data.strategy.Strategy;
 
-import javax.validation.constraints.AssertTrue;
-import java.math.BigInteger;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class StrategyTest {
+    private TestIdentifier identifier;
     private Strategy strategy;
-    //private LocalDateTime start;
+    private LocalDateTime start;
     private LocalDateTime end;
     private RawReport rawReport;
-    private BigInteger reportId;
+    private Long reportId;
 
-    public StrategyTest(Strategy strategy, LocalDateTime end, double funds, BigInteger reportId) {
+    public StrategyTest(String username, Strategy strategy,LocalDateTime start, LocalDateTime end, Long reportId) {
+       this.identifier =new TestIdentifier(username, strategy.getName());
         this.strategy = strategy;
+        this.start=start;
         this.end = end;
-        this.rawReport=new RawReport(funds);
+        this.rawReport=new RawReport(strategy.getFunds());
         this.reportId = reportId;
+    }
+
+    public StrategyTest(TestIdentifier identifier) {
+        this.identifier = identifier;
+        strategy=null;
+        start=null;
+        end=null;
+        rawReport=null;
+        reportId=null;
+    }
+
+    public TestIdentifier getIdentifier() {
+        return identifier;
+    }
+
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public RawReport getRawReport() {
+        return rawReport;
+    }
+
+    public Long getReportId() {
+        return reportId;
+    }
+
+    public Asset getAsset(){
+        return strategy.getAsset();
+    }
+
+    public String getStrategyName(){
+        return strategy.getName();
     }
 
     public void execute(double price){
         //TODO
     }
 
-    public Asset getAsset(){
-        return strategy.getAsset();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StrategyTest that = (StrategyTest) o;
+        return Objects.equals(identifier, that.identifier);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identifier);
     }
 }
