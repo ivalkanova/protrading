c0ba2adfab04c5dcd93e5d31463ef8756ab7364f
Ivelina <ivelinavul@gmail.com>
2020-07-13 10:53:59 +0300

    Add DataSuplier implementation and tests

--- src/main/java/com/trading/protrading/marketdata/DataSupplier.java
+++ src/main/java/com/trading/protrading/marketdata/DataSupplier.java
@@ -1,21 +1,93 @@
 package com.trading.protrading.marketdata;
 
+import com.trading.protrading.data.strategy.Asset;
 import com.trading.protrading.data.strategy.Quote;
+import com.trading.protrading.exceptions.InvalidPeriodException;
 import com.trading.protrading.generators.QuoteGenerator;
+
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class DataSupplier {
 
+    private final Map<Asset, Archive> archives;
     private final QuoteGenerator generator;
+    private Quote latestQuote;
+    private ScheduledExecutorService updateSheduler;
 
     public DataSupplier() {
-        this(new QuoteGenerator());
+        this(new EnumMap<>(Asset.class), new QuoteGenerator());
+        configureArchives();
     }
 
-    DataSupplier(QuoteGenerator generator) {
+    DataSupplier(Map<Asset, Archive> archives, QuoteGenerator generator) {
+        this.archives = archives;
         this.generator = generator;
+        this.latestQuote = generator.generateQuote();
+        configureLatestQuoteUpdate();
     }
 
-    public Quote getLatestQuote() {
-        return generator.generateQuote();
+    private void configureArchives() {
+        String workingDirectory = ".";
+        String archiveDirectory = "quotes-archive";
+        Path location = Path.of(workingDirectory, archiveDirectory);
+        for (Asset asset : Asset.values()) {
+            archives.put(asset, new Archive(asset, location));
+        }
     }
+
+    private void configureLatestQuoteUpdate() {
+        int poolSize = 1;
+        updateSheduler = Executors.newScheduledThreadPool(poolSize);
+        int period = 5;
+        long delay = 0;
+        updateSheduler.scheduleAtFixedRate(this::updateLatestQuote, delay, period,
+                TimeUnit.SECONDS);
+    }
+
+    public synchronized Quote getLatestQuote() {
+        return latestQuote;
+    }
+
+    private void updateLatestQuote() {
+        Quote quote = generator.generateQuote();
+        synchronized (this) {
+            latestQuote = quote;
+        }
+        Archive archive = archives.get(quote.getAsset());
+        archive.addLatestQuote(quote);
+
+    }
+
+    public List<Quote> getOldQuotes(LocalDateTime start, LocalDateTime end, Asset asset)
+            throws InvalidPeriodException {
+        LocalDate today = LocalDate.now();
+        if (today.equals(start.toLocalDate()) || today.equals(end.toLocalDate())) {
+            throw new InvalidPeriodException("Parameters start and end cannot be todays date");
+        }
+        Archive archive = archives.get(asset);
+        List<Quote> allQuotes = archive.getOldQuotes(start.toLocalDate(), end.toLocalDate());
+        return filterQuotes(allQuotes, start, end);
+    }
+
+    private List<Quote> filterQuotes(List<Quote> quotes, LocalDateTime start, LocalDateTime end) {
+        return quotes.stream()
+                .filter((element) -> !element.getDate().isBefore(start) && !element.getDate()
+                        .isAfter(end))
+                .collect(Collectors.toList());
+    }
+
+    public void stopLatestQuoteUpdate() {
+        updateSheduler.shutdown();
+    }
+
 }
+
---
+++ src/test/java/com/trading/protrading/marketdata/DataSupplierTest.java
@@ -0,0 +1,72 @@
+package com.trading.protrading.marketdata;
+
+import com.trading.protrading.data.strategy.Asset;
+import com.trading.protrading.data.strategy.Quote;
+import com.trading.protrading.data.strategy.TransactionType;
+import com.trading.protrading.exceptions.InvalidPeriodException;
+import com.trading.protrading.generators.QuoteGenerator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+
+public class DataSupplierTest {
+
+    @Mock
+    private QuoteGenerator generator;
+    @Mock
+    private Archive goldArchive;
+    private static List<Quote> goldQuotes;
+
+    @BeforeAll
+    public static void setUp() {
+        LocalDateTime startDate = LocalDateTime.of(2020, 6, 3, 3, 30, 30);
+        LocalDateTime endDate = startDate.plusDays(3);
+        goldQuotes = new ArrayList<>();
+        goldQuotes.add(new Quote(Asset.GOLD, 20, TransactionType.BUY, startDate));
+        goldQuotes.add(new Quote(Asset.GOLD, 20, TransactionType.BUY, endDate));
+        goldQuotes.add(new Quote(Asset.GOLD, 20, TransactionType.BUY, endDate));
+        goldQuotes.add(new Quote(Asset.GOLD, 20, TransactionType.BUY, endDate.plusMinutes(3)));
+
+    }
+
+    @Test
+    public void getOldQuotes() throws InvalidPeriodException {
+        Map<Asset, Archive> archives = new HashMap<>();
+        archives.put(Asset.GOLD, goldArchive);
+        DataSupplier dataSupplier = new DataSupplier(archives, generator);
+        LocalDateTime startDate = LocalDateTime.of(2020, 6, 3, 3, 30, 30);
+        LocalDateTime endDate = startDate.plusDays(3);
+        when(goldArchive.getOldQuotes(startDate.toLocalDate(), endDate.toLocalDate()))
+                .thenReturn(goldQuotes);
+        List<Quote> expected = goldQuotes;
+        expected.remove(goldQuotes.size() - 1);
+        assertIterableEquals(dataSupplier.getOldQuotes(startDate, endDate, Asset.GOLD), expected,
+                "The actual quotes are not in the range start end");
+    }
+
+    @Test
+    public void getOldQuotesEndIsToday() {
+        DataSupplier dataSupplier = new DataSupplier();
+
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime start = today.minusDays(1);
+        assertThrows(InvalidPeriodException.class,
+                () -> dataSupplier.getOldQuotes(start, today, Asset.GOLD),
+                "Start or end cannot be today");
+    }
+
+}
