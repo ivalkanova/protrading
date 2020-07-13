9e374bf0632f53e83ffa680bde12d58f81be2f0d
ivantenevvasilev <ivantenevvasilev@abv.bg>
2020-07-13 20:59:20 +0300

    Implemented StrategyService

--- src/main/java/com/trading/protrading/service/StrategyService.java
+++ src/main/java/com/trading/protrading/service/StrategyService.java
@@ -2,50 +2,125 @@ package com.trading.protrading.service;
 
 
 import com.trading.protrading.demotesting.RealTimeStrategyTestingTasksStorage;
+import com.trading.protrading.dto.RuleDTO;
+import com.trading.protrading.dto.StrategyDTO;
+import com.trading.protrading.model.Account;
+import com.trading.protrading.model.Condition;
+import com.trading.protrading.repository.*;
 import com.trading.protrading.strategytesting.TestConfiguration;
 import com.trading.protrading.exceptions.StrategyNotFoundException;
 import com.trading.protrading.model.Rule;
 import com.trading.protrading.model.Strategy;
-import com.trading.protrading.repository.ReportRepository;
-import com.trading.protrading.repository.StrategyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class StrategyService {
 
+    private AccountRepository accountRepository;
     private StrategyRepository strategyRepository;
     private ReportRepository reportRepository;
+    private ConditionRepository conditionRepository;
+    private RuleRepository ruleRepository;
+
+
     private RealTimeStrategyTestingTasksStorage tester;
 
-    public StrategyService(StrategyRepository repository, ReportRepository reportRepository) {
-        this.strategyRepository = repository;
+    public StrategyService(AccountRepository accountRepository, StrategyRepository strategyRepository, ReportRepository reportRepository, ConditionRepository conditionRepository, RuleRepository ruleRepository) {
+        this.accountRepository = accountRepository;
+        this.strategyRepository = strategyRepository;
         this.reportRepository = reportRepository;
+        this.conditionRepository = conditionRepository;
+        this.ruleRepository = ruleRepository;
     }
 
-    public void create(Strategy strategy) {
+    public void create(StrategyDTO strategy, String username) {
+        Optional<Account> account = this.accountRepository.findFirstByUserName(username);
+        if (account.isEmpty()) {
+            // TODO THROW
+        }
+        Strategy databaseStrategyModel = new Strategy();
+
+        databaseStrategyModel.setName(strategy.getName());
+        databaseStrategyModel.setUser(account.get());
+        this.strategyRepository.save(databaseStrategyModel);
+
+        Set<Rule> rules = strategy.getRules()
+                .stream()
+                .map(this::mapFromDto)
+                .collect(Collectors.toSet());
+        for (Rule rule : rules) {
+            rule.setStrategy(databaseStrategyModel);
+        }
+        databaseStrategyModel.setRules(rules);
+
+        Set<Condition> conditions = databaseStrategyModel.getRules().stream().map(Rule::getCondition).collect(Collectors.toSet());
+        this.conditionRepository.saveAll(conditions);
+        this.ruleRepository.saveAll(rules);
+
+
+        this.strategyRepository.save(databaseStrategyModel);
     }
 
-    public void delete(String userName, String strategyName) {
+    public void delete(String userName, String strategyName) throws StrategyNotFoundException {
+        Optional<Strategy> strategy = this.strategyRepository.getFirstByNameAndUser_UserName(userName, strategyName);
+        if (strategy.isEmpty()) {
+            throw new StrategyNotFoundException("Can not delete strategy " + strategyName + " as it doesn't exist for user " + userName);
+        }
+
+        this.strategyRepository.delete(strategy.get());
     }
 
-    public Strategy get(String userName, String strategyName) {
-        return null;
+    public Strategy get(String userName, String strategyName) throws StrategyNotFoundException {
+        Optional<Strategy> strategy = this.strategyRepository.getFirstByNameAndUser_UserName(userName, strategyName);
+        if (strategy.isEmpty()) {
+            throw new StrategyNotFoundException("Strategy " + strategyName + "not found for user " + userName);
+        }
+
+        return strategy.get();
     }
 
     public List<Strategy> getAll(String userName) {
-        return null;
+        return this.strategyRepository.getAllByUser_UserName(userName);
     }
 
-    public void changeName(String userName, String oldName, String newName) {
+    public void changeName(String userName, String oldName, String newName) throws StrategyNotFoundException {
+        Optional<Strategy> strategy = this.strategyRepository.getFirstByNameAndUser_UserName(userName, oldName);
+        if (strategy.isEmpty()) {
+            throw new StrategyNotFoundException("Strategy with name " + oldName + " was not found for user " + userName);
+        }
+        strategy.get().setName(newName);
+        this.strategyRepository.save(strategy.get());
     }
 
-    public void changeRules(String userName, String strategyName, List<Rule> rules) {
-    }
+    public void changeRules(String userName, String strategyName, Set<RuleDTO> rules) throws StrategyNotFoundException {
+        Optional<Strategy> strategy = this.strategyRepository.getFirstByNameAndUser_UserName(strategyName, userName);
+        if (strategy.isEmpty()) {
+            throw new StrategyNotFoundException("Strategy with name " + strategyName + "for user" + userName + "not found and couldn't be updated");
+        }
+        Set<Rule> oldRules = strategy.get().getRules();
+        Set<Condition> oldConditions = oldRules.stream().map(Rule::getCondition).collect(Collectors.toSet());
 
-    public void changeTransactionBuyFunds(String userName, String strategyName, double transactionBuyFunds) {
+        Set<Rule> ruleDatabaseModels = rules.stream().map(this::mapFromDto).collect(Collectors.toSet());
+        for (Rule rule : ruleDatabaseModels) {
+            rule.setStrategy(strategy.get());
+        }
+
+        strategy.get().setRules(ruleDatabaseModels);
+
+        Set<Condition> conditionDatabaseModels = ruleDatabaseModels.stream().map(Rule::getCondition).collect(Collectors.toSet());
+
+        this.conditionRepository.saveAll(conditionDatabaseModels);
+        this.ruleRepository.saveAll(ruleDatabaseModels);
+        this.ruleRepository.deleteAll(oldRules);
+        this.conditionRepository.deleteAll(oldConditions);
+
+        this.strategyRepository.save(strategy.get());
     }
 
     public UUID enableStrategy(TestConfiguration testConfiguration)
@@ -66,4 +141,16 @@ public class StrategyService {
         tester.disableStrategy(username, strategy);
     }
 
+
+    private Rule mapFromDto(RuleDTO ruleDTO) {
+        Rule r = new Rule();
+        r.setStopLoss(ruleDTO.getStopLoss());
+        r.setTakeProfit(ruleDTO.getTakeProfit());
+        Condition c = new Condition();
+        c.setAssetPrice(ruleDTO.getCondition().getAssetPrice());
+        c.setPredicate(ruleDTO.getCondition().getPredicateEnum());
+
+        r.setCondition(c);
+        return r;
+    }
 }
