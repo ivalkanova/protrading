4512b13efda39c2d109d8ded847522fe5cb51357
ivantenevvasilev <ivantenevvasilev@abv.bg>
2020-07-18 21:38:40 +0300

    Implemented menu outlines.

---
+++ src/main/java/com/trading/protrading/client/commands/TestingMenuCommand.java
@@ -0,0 +1,35 @@
+package com.trading.protrading.client.commands;
+
+import com.trading.protrading.client.ClientInMemoryStorage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.http.HttpClient;
+
+public class TestingMenuCommand extends Command {
+    public TestingMenuCommand(HttpClient httpClient, PrintWriter writer, BufferedReader reader, ClientInMemoryStorage storage) {
+        super(httpClient, writer, reader, storage);
+    }
+
+    @Override
+    public void run(String url) throws IOException {
+        writer.println("Available commands:");
+        writer.println("Run-strategy");
+        writer.println("Disable-strategy");
+        writer.println("Compare-strategies");
+        writer.println("Please choose a menu or type \"Back\" to go to the previous menu. ");
+
+        String line = reader.readLine();
+        switch (line.toLowerCase().trim()) {
+            case "back":
+                return;
+            case "run-strategy":
+
+            case "disable-strategy":
+                    new DisableStrategyCommand(httpClient, writer, reader, storage).run(url);
+                break;
+            case "compare-strategies":
+        }
+    }
+}
